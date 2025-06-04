10ea3097e6c5e707df872f5ea3201f52e0736748
AruukeNurpaiaskyzy <aruukelisa@gmail.com>
2025-06-04 16:42:29 +0600

    dependancy inversion3

---
+++ main3.py
@@ -0,0 +1,15 @@
+class NewsSourse:
+    def get_news(self):
+        pass
+class GoogleNewsAPI(NewsSourse):
+    def get_news(self):
+        return ['Новость1', 'новость2']
+class LocalNewsAPI(NewsSourse):
+    def get_news(self):
+        return ["Местная новость A", "Местная новость B"]
+class NewsWidget:
+    def __init__(self, source: NewsSourse):
+        self.source = source  # Зависит от абстракции, не от реализации
+    def render(self):
+        news = self.source.get_news()
+        print(news)
